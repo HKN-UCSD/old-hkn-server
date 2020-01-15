1927823e32d9a5f7642833a1edd5c080a729dd91
AneeshJalan <akjalan@ucsd.edu>
2020-01-15 01:20:15 -0800

    Added minor check to queryCurrentUserRole

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -81,7 +81,7 @@ class Firebase {
 
                        return data.role_id
                    })
-                   .catch(error => {console.log('ERROR:' + error)})
+                   .catch(error => {console.log('ERROR: ' + error)})
     }
 
     getRoleFromID = (roleID) => {
@@ -105,6 +105,9 @@ class Firebase {
     queryCurrentUserRole = () => {
         return this.getUserRoleID()
                .then(roleID => {
+                   if(roleID === null || roleID === undefined) {
+                       throw Error("User does not have Role");
+                   }
                    return this.getRoleFromID(roleID)
                })
                .catch(error => {console.log('ERROR: ' + error)})
