e615181d11c5caecf3d450d09c747e6279730dc1
Eric Ke <keric.yi@gmail.com>
2020-04-10 12:02:45 -0700

    setup temp

---
+++ src/services/Firebase/setup.js
@@ -0,0 +1,20 @@
+import * as firebase from 'firebase/app';
+import 'firebase/firestore';
+
+const config = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+};
+
+firebase.initializeApp(config);
+firebase
+  .firestore()
+  .enablePersistence()
+  .catch(err => {
+    throw Error(`Firestore enable persistence failed: ${err}`);
+    // Just don't cache. Catching error to ensure that it doesn't propagate out.
+  });
