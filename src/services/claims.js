8e7f00d14d3e6731001458050417aa59cbaed425
gypang <gypang24@gmail.com>
2020-05-01 19:23:16 -0700

    Adds hacky singleton service for checking roles.
    Firebase Auth, as of May 1st 2020, is one of the most unpleasant pieces
    of software that I've had the chance to work with. There's no good way
    to store role information so you're forced to jam role information into
    their custom claims which is more like a hot pile of unusable mess as
    there's no easy way to check custom claims since it comes with
    getIDTokenResult. (Who even came up with that name?!)
    
    Anyways, this commit adds in a hacky singleton service to imitate a
    Redux Store because relying on the Firebase Auth state change listener
    was too slow and caused a lot of bugs. Hopefully no one has to ever read
    this code again but if you do I'm terribly sorry.

---
+++ src/services/claims.js
@@ -0,0 +1,27 @@
+// Singleton redux-like service. This exists because Firebase Auth SUCKS
+// and won't give us claims in the authDidChange call.
+// This literally just has get set calls for the claims of the currently logged in user
+// ClaimsSingleton is a HACK around Firebase Auth's inability to return a clean user object with
+// claims. Please don't use this singleton outside of what's currently been implemented
+// There's a Context that wraps these claims for normal use.
+
+let claims = {};
+
+const ClaimsSingleton = {
+  setClaims: newClaims => {
+    claims = Object.keys(newClaims);
+  },
+  getClaims: () => claims,
+};
+Object.freeze(ClaimsSingleton);
+
+const hasClaim = claim => userClaims => {
+  return userClaims.includes(claim);
+};
+
+// These functions take in the current user claims as an object
+const isOfficer = hasClaim('Officer');
+const isInductee = hasClaim('Inductee');
+const isMember = hasClaim('Member');
+
+export { ClaimsSingleton, isOfficer, isInductee, isMember };
