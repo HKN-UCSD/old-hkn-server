c4ebd0f29beec98775b6c5b7d1bda01ea60ae09f
thaigillespie <ttgilles@ucsd.edu>
2020-06-18 11:15:49 -0700

    Made sure that everything still works after updating environment variables

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -19,5 +19,5 @@ import './commands';
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
 Cypress.Screenshot.defaults({
-  screenshotOnRunFailure: false,
+  screenshotOnRunFailure: true,
 });
