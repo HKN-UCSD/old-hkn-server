2e62a6ee38d39b9802c5aeeb6f261e39de9128b7
thaigillespie <ttgilles@ucsd.edu>
2020-06-22 20:47:05 -0700

    Finished the frontend of the sign up page, besides making HTTP requests to send in inputted data from the user to the db (need to wait for backend on that first)

---
+++ src/constants/eligibleMajors.js
@@ -0,0 +1,36 @@
+const ELIGIBLE_MAJORS = {
+  BENG: {
+    BE: 'Bioengineering',
+  },
+  COGS: {
+    COGS_ML: 'Cognitive Science - ML',
+  },
+  CSE: {
+    CE: 'Computer Engineering',
+    CS: 'Computer Science',
+    CS_BIOINFORMATICS: 'CS - Bioinformatics',
+  },
+  DSC: {
+    DS: 'Data Science',
+  },
+  ECE: {
+    EE_AND_SOC: 'EE and Society',
+    CE: 'Computer Engineering',
+    EE: 'Electrical Engineering',
+    EP: 'Engineering Physics',
+  },
+  MAE: {
+    AE: 'Aerospace Engineering',
+    ENV_ENG: 'Environmental Engineering',
+    ME: 'Mechanical Engineering',
+  },
+  MATH: {
+    MATH_CS: 'Math - Computer Science',
+  },
+  NANO: {
+    CHEM_ENG: 'Chemical Engineering',
+    NANO_ENG: 'NanoEngineering',
+  },
+};
+
+export default ELIGIBLE_MAJORS;
