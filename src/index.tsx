b67c942550d40e93bd90e914e57e2a0b75b7a75f
gypang <gypang24@gmail.com>
2020-08-18 14:31:23 -0700

    Convert to default import

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import * as firebase from 'firebase/app';
+import firebase from 'firebase/app';
 import 'firebase/firestore';
 
 import * as serviceWorker from './serviceWorker';
