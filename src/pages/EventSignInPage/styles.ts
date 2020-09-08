15241f69724ffc296c975ed3fee45ce2b89abae7
gypang <gypang24@gmail.com>
2020-09-07 21:15:50 -0700

    Migrate EventSignInForm to useParam hook + typescript.

--- src/pages/EventSignInPage/styles.ts
+++ src/pages/EventSignInPage/styles.ts
@@ -1,4 +1,6 @@
-const styles = () => ({
+import { makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles({
   root: {
     display: 'flex',
     flexDirection: 'column',
@@ -22,4 +24,4 @@ const styles = () => ({
   },
 });
 
-export default styles;
+export default useStyles;
