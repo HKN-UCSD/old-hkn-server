1158d09f1cb8b1575976b161385e89848daa2350
gypang <gypang24@gmail.com>
2020-09-09 02:26:00 -0700

    Add border, grid, remove ripple.

--- src/components/cards/CardWithVerticalTabs.tsx
+++ src/components/cards/CardWithVerticalTabs.tsx
@@ -1,5 +1,11 @@
 import React, { useState } from 'react';
-import { Grid, Tabs as MuiTabs, Tab as MuiTab } from '@material-ui/core';
+import {
+  Grid,
+  Tabs as MuiTabs,
+  Tab as MuiTab,
+  makeStyles,
+  Theme,
+} from '@material-ui/core';
 
 import { Card } from './Card';
 
@@ -7,22 +13,32 @@ export interface CardWithVerticalTabsProps {
   items: { title: string; element: JSX.Element }[];
 }
 
+const useStyles = makeStyles((theme: Theme) => ({
+  tabs: { borderRight: `1px solid ${theme.palette.divider}` },
+}));
+
 export function CardWithVerticalTabs({ items }: CardWithVerticalTabsProps) {
   const [index, setIndex] = useState(0);
+  const classes = useStyles();
 
   const handleChange = (_: React.ChangeEvent<unknown>, newValue: number) => {
     setIndex(newValue);
   };
 
   const tabElements: JSX.Element[] = items.map(item => (
-    <MuiTab label={item.title} key={item.title} />
+    <MuiTab disableRipple label={item.title} key={item.title} />
   ));
 
   return (
     <Card>
       <Grid container spacing={2}>
         <Grid item>
-          <MuiTabs orientation='vertical' value={index} onChange={handleChange}>
+          <MuiTabs
+            orientation='vertical'
+            value={index}
+            onChange={handleChange}
+            className={classes.tabs}
+          >
             {tabElements}
           </MuiTabs>
         </Grid>
