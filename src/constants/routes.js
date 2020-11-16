d37429f9d627c7b042ab7ff3fe7d45960d8b1aa0
Godwin Pang <gypang24@gmail.com>
2020-11-15 23:30:00 -0800

    Display induction requirements for user. (#194)
    * Update points page to be compatible with new API.
    
    * Display error message on no points.
    
    * Add authentication + security for page.
    
    * Update cypress test to use new points endpoint.

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -4,7 +4,7 @@ export const SIGN_IN = '/login';
 export const CALENDAR = '/calendar';
 export const HOME = '/';
 export const EVENTS = '/events';
-export const POINTS = '/points';
+export const POINTS = '/points/:id';
 export const RESUME = '/resume';
 export const INDUCTEES = '/inductees';
 export const EVENT_EDIT = '/events/:eventId/edit';
@@ -18,5 +18,6 @@ export const EVENT_RSVP = '/events/:id/rsvp';
 
 export const PROFILE_WITH_ID = id => `/profile/${id}`;
 export const PROFILE_EDIT_WITH_ID = id => `/profile/${id}/edit`;
+export const POINTS_WITH_ID = id => `/points/${id}`;
 
 export const CURR_USER_ID_ALIAS = 'me';
