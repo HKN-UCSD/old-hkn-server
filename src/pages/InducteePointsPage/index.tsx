8a5556af4032a356521949fd5010fdab5f70f2c1
gypang <gypang24@gmail.com>
2020-11-16 01:30:20 -0800

    Add link button to inductee points table.
    Adds a button to link to detailed view of inductee points.

--- src/pages/InducteePointsPage/index.tsx
+++ src/pages/InducteePointsPage/index.tsx
@@ -1,7 +1,10 @@
 import React, { useState, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+import PersonIcon from '@material-ui/icons/Person';
 
 import useStyles from './styles';
 
+import { POINTS_WITH_ID } from '@Constants/routes';
 import { Table } from '@SharedComponents';
 import { getAllInducteePoints, InducteePoint } from '@Services/PointsService';
 
@@ -26,6 +29,7 @@ export default function InducteePointsPage() {
   >(undefined);
 
   const classes = useStyles();
+  const history = useHistory();
 
   useEffect(() => {
     const getInducteePoints = async () => {
@@ -35,13 +39,27 @@ export default function InducteePointsPage() {
     getInducteePoints();
   }, []);
 
+  const actions = [
+    {
+      icon: () => <PersonIcon />,
+      tooltip: 'See more',
+      onClick: (_: unknown, row: InducteePoint) =>
+        history.push(POINTS_WITH_ID(row.user)),
+    },
+  ];
+
   return (
     <div className={classes.root}>
       <Table
         columns={columns}
         data={inducteePoints}
+        actions={actions}
         title='Inductee Points'
-        options={{ filtering: true, exportButton: true }}
+        options={{
+          filtering: true,
+          exportButton: true,
+          actionsColumnIndex: -1,
+        }}
       />
     </div>
   );
